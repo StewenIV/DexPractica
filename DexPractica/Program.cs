ce95fc24c24318c51e5a0f82b9d9e48ddd2374b2
StewenIV <amacarin@mail.ru>
2025-06-19 14:25:13 +0300

    feat: implement anime data model and prediction logic

--- DexPractica/Program.cs
+++ DexPractica/Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DexPractica.AnimeData;
+using Microsoft.ML;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        var context = new MLContext();
+        var path = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)
+            .Parent
+            .Parent
+            .FullName, "Data", "anime.csv");
+        
+        var data = context.Data.LoadFromTextFile<AnimeData>(path
+            ,hasHeader:true, separatorChar:',');
+        
+        var split = context.Data.TrainTestSplit(data, testFraction: 0.2);
+        
+        var pipeline = context.Transforms.Categorical.OneHotEncoding("Genre")
+            .Append(context.Transforms.Categorical.OneHotEncoding("Type"))
+            .Append(context.Transforms.Concatenate("FeaturesRaw", "Genre", "Type", "Episodes", "Members"))
+            .Append(context.Transforms.NormalizeMinMax("Features", "FeaturesRaw"))
+            .Append(context.Regression.Trainers.Sdca(labelColumnName: "Rating", featureColumnName: "Features"));
+
+        var model = pipeline.Fit(split.TrainSet);
+        
+        var predictions = model.Transform(split.TestSet);
+        var metrics = context.Regression.Evaluate(predictions, labelColumnName: "Rating");
+
+        Console.WriteLine($"R^2: {metrics.RSquared:0.##}");
+        Console.WriteLine($"MAE: {metrics.MeanAbsoluteError:0.##}");
+        Console.WriteLine($"RMSE: {metrics.RootMeanSquaredError:0.##}");
+
+        // 6. Прогноз для новых данных
+        var engine = context.Model.CreatePredictionEngine<AnimeData, AnimePrediction>(model);
+
+        var example = new AnimeData
+        {
+            Name = "MyAnime",
+            Genre = "Action, Comedy",
+            Type = "TV",
+            Episodes = 12,
+            Members = 50000
+        };
+
+        var prediction = engine.Predict(example);
+        Console.WriteLine($"Предсказанный рейтинг: {prediction.PredictedRating:0.00}");
+    }
+}
--- DexPractica/obj/Debug/net8.0/DexPractica.AssemblyInfo.cs
+++ DexPractica/obj/Debug/net8.0/DexPractica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DexPractica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5040c2ea0ea7a902a3f1f935bc13c707d294366")]
 [assembly: System.Reflection.AssemblyProductAttribute("DexPractica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DexPractica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
